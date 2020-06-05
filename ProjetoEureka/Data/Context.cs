a05caca1a54d2963be4566a1882ff07543ca0043
André Moraes <dedehmoraes@hotmail.com>
2020-06-05 19:50:04 -0300

    Conexao com o banco

---
+++ ProjetoEureka/Data/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjetoEureka.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjetoEureka.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions <Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Noticia> euk_noticia { get; set; }
+        public DbSet<Categoria> euk_categoria { get; set; }
+
+    }
+}
--- ProjetoEureka/Startup.cs
+++ ProjetoEureka/Startup.cs
@@ -5,9 +5,13 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ProjetoEureka.Data;
+using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ProjetoEureka
 {
@@ -23,7 +27,11 @@ namespace ProjetoEureka
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<Context>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
