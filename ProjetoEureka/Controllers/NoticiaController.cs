5fbbb823488fd1b6a3e82f7fc12b2bba3ca1fff7
Eduardo Silva <easilva91@gmail.com>
2020-06-06 22:46:01 +0100

    New pages

--- ProjetoEureka/Controllers/NoticiaController.cs
+++ ProjetoEureka/Controllers/NoticiaController.cs
@@ -3,16 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjetoEureka.Data;
 
 namespace ProjetoEureka.Controllers
 {
     
     public class NoticiaController : Controller
     {
-       [HttpGet,Route("Noticia/{id}-{titulo}")]
+        private readonly Context _context;
+        public NoticiaController(Context context)
+        {
+            _context = context;
+        }
+        [HttpGet,Route("Noticia/{id}-{titulo}")]
         public IActionResult Index(int id, string titulo)
         {
-            return View();
+            var noticia = _context.euk_noticia.First(x => x.Id == id);
+            return View(noticia);
         }
 
     }
